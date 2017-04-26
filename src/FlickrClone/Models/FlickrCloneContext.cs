9f2310d5c8b9708ebc51d1938505cf534b284c99
Cassie Musolf and Shruti Priya <cassie+shruti@>
2017-04-26 09:11:46 -0700

    setup basic files with entity framework and test project

---
+++ src/FlickrClone/Models/FlickrCloneContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FlickrClone.Models
+{
+    public class FlickrCloneContext : DbContext
+    {
+        public virtual DbSet<Image> Images { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=FlickrClone;integrated security=True");
+        }
+    }
+}
---
+++ src/FlickrClone/Models/Image.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlickrClone.Models
+{
+    [Table("Images")]
+    public class Image
+    {
+        public int ImageId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTaken { get; set; }
+    }
+}
