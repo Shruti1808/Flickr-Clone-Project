a45caddaf1274861dd0ce9e38de2e56218caa845
Cassie Musolf and Shruti Priya <cassie+shruti@>
2017-04-26 11:26:29 -0700

    fix test dependency conflict error and ran our first test

--- FlickrClone.Tests/ImageTest.cs
+++ FlickrClone.Tests/ImageTest.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FlickrClone.Models;
+using Xunit;
 
 namespace FlickrClone.Tests
 {
     public class ImageTest
-    {
+     {
         [Fact]
-        public void GetTitle
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var image = new Image();
+
+            //Act
+            var result = image.Description;
+
+            //Assert
+            Assert.Equal("Wash the dog", result);
+        }
     }
 }
